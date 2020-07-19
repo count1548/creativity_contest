9160c65d2f7f297b97550c9e93dd98ec5aa7afb7
HanGeonUok <w_lan123@naver.com>
2020-07-19 18:55:13 +0900

    rm-rf

--- routes/api/index.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const db = require("./db");
-
-router.use("/db", db);
-
-module.exports = router;
--- routes/index.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var dbRouter = require("./api/db/index");
-
-router.use("./db", dbRouter);
-
-module.exports = router;
