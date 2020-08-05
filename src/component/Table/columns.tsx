01eb043d14ff75399115d49bd088000e07735fa9
HanGeonUok <w_lan123@naver.com>
2020-08-05 13:08:00 +0900

    8/5

--- src/component/Table/columns.tsx
+++
@@ -1,8 +0,0 @@
-const columns = [
-    { title: 'ID', field: 'id', width:0, hidden:true  },
-    { title: '노선', field: 'name', width:100 },
-    { title: '출발', field: 'start', },
-    { title: '경유', field: 'path', },
-    { title: '도착', field: 'terminus', },    
-]
-export default columns;
